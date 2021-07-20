625af91ee0344052adc3433d882c08e96e576714
JoaoDuarte10 <joaoduarte7341@gmail.com>
2021-07-19 21:43:28 -0300

    Esqueleto com o basico para aplicaçoes em React

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../css/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
